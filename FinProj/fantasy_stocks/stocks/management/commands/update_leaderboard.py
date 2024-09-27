56756f081b5e0fcd9905e068be945e1dada6f541
Chambers Volk <chambersvolk@outlook.com>
2024-09-27 12:54:13 -0700

    sdf

--- FinProj/fantasy_stocks/stocks/management/commands/update_leaderboard.py
+++ FinProj/fantasy_stocks/stocks/management/commands/update_leaderboard.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from stocks.models import UserProfile  # Adjust import as needed
+from accounts.models import UserProfile  # Adjust import as needed
 from django.db.models import F
 
 class Command(BaseCommand):
--- FinProj/fantasy_stocks/stocks/models.py
+++ FinProj/fantasy_stocks/stocks/models.py
@@ -36,3 +36,11 @@ class PortfolioStock(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    initial_balance = models.DecimalField(max_digits=10, decimal_places=2, default=10000.00)
+    total_gain_loss = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return self.user.username
