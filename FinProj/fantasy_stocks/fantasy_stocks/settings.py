ecf30a93e0ba9f1aa1754ef488465a06c72f9a1c
Chambers Volk <chambersvolk@outlook.com>
2024-09-28 10:26:29 -0700

    Django

--- FinProj/fantasy_stocks/fantasy_stocks/settings.py
+++ FinProj/fantasy_stocks/fantasy_stocks/settings.py
@@ -159,3 +159,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
